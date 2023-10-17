5b898de4f82a5e4ef3a3a2bb612fe2fa34d12cc9
sadik Goden <sadikgoden001@gmail.com>
2023-10-17 15:41:41 +0300

    cucumber

---
+++ src/test/java/stepDefinitions/AmazonStepdefinition.java
@@ -0,0 +1,71 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class AmazonStepdefinition {
+    AmazonPage amazonPage=new AmazonPage();
+
+    @Given("kullanici amazonUrl anasayfaya gider")
+    public void kullanici_amazon_url_anasayfaya_gider() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+    }
+    @Given("arama kutusuna Nutella yazip aratir")
+    public void arama_kutusuna_nutella_yazip_aratir() {
+        amazonPage.aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+    }
+    @Then("arama sonuclarinin Nutella icerdigini test eder")
+    public void arama_sonuclarinin_nutella_icerdigini_test_eder() {
+        String expectedIcerik= "Nutella";
+        String actualAramaSonucu =amazonPage.sonucYaziElementi.getText();
+        Assert.assertTrue(actualAramaSonucu.contains(expectedIcerik));
+    }
+    @Then("sayfayi kapatir")
+    public void sayfayi_kapatir() {
+        Driver.closeDriver();
+    }
+
+
+    @And("arama kutusuna Java yazip aratir")
+    public void aramaKutusunaJavaYazipAratir() {
+        amazonPage.aramaKutusu.sendKeys("Java"+Keys.ENTER);
+
+    }
+
+    @Then("arama sonuclarinin Java icerdigini test eder")
+    public void aramaSonuclarininJavaIcerdiginiTestEder() {
+        String expectedIcerik= "Java";
+        String actualAramaSonucu =amazonPage.sonucYaziElementi.getText();
+        Assert.assertTrue(actualAramaSonucu.contains(expectedIcerik));
+    }
+    @Given("kullanici {string} anasayfaya gider")
+    public void kullanici_anasayfaya_gider(String istenenUrl) {
+        Driver.getDriver().get(ConfigReader.getProperty(istenenUrl));
+    }
+    @Then("arama kutusuna {string} yazip aratir")
+    public void arama_kutusuna_yazip_aratir(String arananUrun) {
+        amazonPage.aramaKutusu.sendKeys(arananUrun+Keys.ENTER);
+
+    }
+    @Then("arama sonuclarinin {string} icerdigini test eder")
+    public void arama_sonuclarinin_icerdigini_test_eder(String arananUrun) {
+       String actualSonucYazisi =amazonPage.sonucYaziElementi.getText();
+       Assert.assertTrue(actualSonucYazisi.contains(arananUrun));
+    }
+
+
+    @And("{int} saniye bekler")
+    public void saniyeBekler(int saniye) {
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/resources/features/US1002_ParametreleiAmazonSearch.feature
@@ -0,0 +1,23 @@
+Feature: US1002 Kullanici parametre olarak yazdigi urunu amazonda aratir
+
+  Scenario: TC03 parametre ile amazonda arama
+    Given kullanici "amazonUrl" anasayfaya gider
+    Then arama kutusuna "Java" yazip aratir
+    And  arama sonuclarinin "Java" icerdigini test eder
+    Then 2 saniye bekler
+    And sayfayi kapatir
+
+  Scenario:  TC04 parametre ile apple üretir
+    Given kullanici "amazonUrl" anasayfaya gider
+    Then arama kutusuna "apple" yazip aratir
+    And  arama sonuclarinin "apple" icerdigini test eder
+    Then 5 saniye bekler
+    And sayfayi kapatir
+
+
+  git init
+  git add .
+  git commit -m "cucumber"
+  git branch -M main
+  git remote add origin https://github.com/sadikgoden/CUCUMBER.git
+  git push -u origin main
